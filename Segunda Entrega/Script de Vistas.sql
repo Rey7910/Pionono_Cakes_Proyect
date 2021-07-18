dccfeb621231f2a7cf1031c050d55ce4b884c6cf
Rey7910 <70295723+Rey7910@users.noreply.github.com>
2021-07-18 14:59:10 -0500

    Update Script de Vistas.sql

--- Segunda Entrega/Script de Vistas.sql
+++ Segunda Entrega/Script de Vistas.sql
@@ -8,8 +8,8 @@ use pionono_cakes;
 
 drop view if exists adm_clientes;
 drop view if exists adm_contratos;
-drop view if exists info_empleado;
-drop view if exists info_contrato;
+drop view if exists info_funcionarios;
+drop view if exists info_contratos;
 
 
 -- Nota: El Jefe de la microempresa tiene acceso a todas las tablas y vistas de la base de datos
@@ -20,18 +20,19 @@ drop view if exists info_contrato;
 create view adm_clientes as select Nombre, Apellido, Perfil from cliente;
 select * from adm_clientes;
 
--- Vista de los datos de los contratos de los empleados, accesible para administradores de sucursales
+-- Vista de los datos de los contratos de los empleados, accesible para administradores de sucursales (sin acceso a salario)
 create view adm_contratos as select empleado.Nombre, empleado.Apellido, 
-contrato.cargo, contrato.fecha_contratacion, contrato.fecha_terminacion 
-from empleado, contrato where empleado.idContrato = contrato.idContrato;
+cargos.Cargo, contrato.fecha_contratacion, contrato.fecha_terminacion 
+from empleado, contrato, cargos where empleado.idContrato = contrato.idContrato and cargos.idCargo=contrato.idCargo;
 
--- 2) VISTAS PARA EMPLEADOS
--- Vista para acceder a la información personal del propio empleada (condicionada desde la interfaz), 
+-- 2) VISTAS PARA FUNCIONARIOS
+-- Vista para acceder a la información personal del propio funcionario (condicionada desde la interfaz), 
 -- en caso de ser administrador de sucursal, puede acceder a la información de todos los empleados 
-create view info_empleado as select Nombre, Apellido, Direccion, 
+create view info_funcionarios as select Nombre, Apellido, Direccion, 
 Telefono, EPS, Ciudad, Fecha_de_Nacimiento from empleado;
 
--- Vista de los datos del contrato de cada empleado, solo tiene acceso al registro del mismo (condicionado desde inerfaz)
-create view info_contrato as select empleado.Nombre, empleado.Apellido, 
-contrato.cargo, contrato.salario, contrato.fecha_contratacion, contrato.fecha_terminacion 
-from empleado, contrato where empleado.idContrato = contrato.idContrato;
+-- Vista de los datos del contrato de cada funcionario, el funcionario solo puede acceder a su propio registro (condicionado desde inerfaz)
+create view info_contratos as select empleado.Nombre, empleado.Apellido, 
+cargos.cargo, contrato.salario, contrato.fecha_contratacion, contrato.fecha_terminacion 
+from empleado, contrato, cargos where empleado.idContrato = contrato.idContrato and cargos.idCargo=contrato.idCargo;
+
