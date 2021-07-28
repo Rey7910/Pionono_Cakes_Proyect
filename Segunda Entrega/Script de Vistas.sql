636fac646538506a5670c86d608c9aa5c79a729c
santiagohch <80603960+shchUNAL@users.noreply.github.com>
2021-07-27 20:06:51 -0500

    Update Script de Vistas.sql

--- Segunda Entrega/Script de Vistas.sql
+++ Segunda Entrega/Script de Vistas.sql
@@ -11,6 +11,7 @@ drop view if exists adm_contratos;
 drop view if exists info_empleados;
 drop view if exists info_contratos;
 drop view if exists insumos_domicilio;
+drop view if exists sucursales_info;
 
 -- Nota: El Jefe de la microempresa tiene acceso a todas las tablas y vistas de la base de datos
 
@@ -25,14 +26,13 @@ create view adm_contratos as select empleado.Nombre, empleado.Apellido,
 cargos.Nombre as cargo, contrato.fecha_contratacion, contrato.fecha_terminacion 
 from empleado, contrato, cargos where empleado.idContrato = contrato.idContrato and cargos.idCargo=contrato.idCargo;
 
--- Vista de productos vendidos ### NUEVA
-drop view if exists productos_vendidos;
-create view productos_vendidos as select producto.nombre as producto, concat(cliente.nombre,' ',cliente.apellido) ,
- concat(empleado.nombre,' ',empleado.apellido), sucursal.nombre, venta.fecha 
-from cliente,empleado,venta,sucursal,producto,venta_productos 
-where cliente.idcliente = venta.idcliente and empleado.idempleado = venta.idempleado 
-and venta_productos.idproducto = producto.idproducto and venta_productos.idventa = venta.idventa
-and venta.idsucursal=sucursal.idsucursal;
+-- Vista de productos vendidos 
+create view productos_vendidos as select Producto.nombre, cliente.nombre as cliente_n, cliente.apellido 
+as cliente_a, empleado.nombre as empleado_n, empleado.apellido as empleado_a,
+sucursal.nombre as sucursal from producto, empleado, cliente, venta_productos, sucursal, venta 
+where venta.idventa = venta_productos.idventa
+and cliente.idcliente = venta.idventa and venta.idempleado = empleado.idempleado and venta.idsucursal = sucursal.idsucursal
+and venta_productos.idproducto = producto.idproducto;
 
 -- Vista de insumos vendidos 
 create view insumos_vendidos as select insumo.nombre, cliente.nombre as cliente_n, cliente.apellido 
@@ -73,14 +73,41 @@ from proveedor;
 create view info_empleados as select Nombre, Apellido, Direccion, 
 Telefono, EPS, Ciudad, Fecha_de_Nacimiento from empleado;
 
+-- Cambios realizados el 27/07
+
 -- Vista de los datos del contrato de cada empleado, el funcionario solo puede acceder a su propio registro (condicionado desde interfaz)
 create view info_contratos as select empleado.Nombre, empleado.Apellido, 
 cargos.nombre as cargo, contrato.salario, contrato.fecha_contratacion, contrato.fecha_terminacion 
 from empleado, contrato, cargos where empleado.idContrato = contrato.idContrato and cargos.idCargo=contrato.idCargo;
 
-
-
-create view sucursales_info as select  sucursal.categoria, sucursal.nombre, sucursal.ubicacion, sucursal.ciudad, 
-concat(empleado.nombre,' ', empleado.apellido) 
+-- Vista para acceder a la información de las sucursales
+create view sucursales_info as select sucursal.categoria, sucursal.nombre,
+sucursal.ubicacion, sucursal.ciudad, concat(empleado.nombre,' ', empleado.apellido)
 from sucursal, empleado where empleado.idempleado = sucursal.administrador;
+
+-- Vista para acceder a la información de las ventas de insumos como administrador
+create view ventas_insumos_adm as select insumo.nombre as Nombre, concat(cliente.nombre,' ',cliente.apellido) as Cliente, 
+concat(empleado.nombre,' ',empleado.apellido) as Empleado, sucursal.nombre as Sucursal, venta.fecha as Fecha_venta from cliente,empleado,venta,sucursal,
+insumo,venta_insumos where cliente.idcliente = venta.idcliente and empleado.idempleado = venta.idempleado and 
+venta_insumos.idinsumo = insumo.idinsumo and venta_insumos.idventa = venta.idventa and venta.idsucursal=sucursal.idsucursal;
+
+-- Vista para acceder a la información de las ventas de productos como administrador
+create view ventas_productos_adm as select producto.nombre as Nombre, concat(cliente.nombre,' ',cliente.apellido) as Cliente, 
+concat(empleado.nombre,' ',empleado.apellido) as Empleado, sucursal.nombre as Sucursal, venta.fecha as Fecha_venta from cliente,empleado,venta,sucursal,producto,venta_productos 
+where cliente.idcliente = venta.idcliente and empleado.idempleado = venta.idempleado and venta_productos.idproducto = producto.idproducto 
+and venta_productos.idventa = venta.idventa and venta.idsucursal=sucursal.idsucursal;
+
+
+-- Vista para acceder a la información de los domicilios de insumos como administrador
+create view domicilios_insumos_adm as select insumo.nombre as Nombre, concat(cliente.nombre,' ',cliente.apellido) as Cliente, 
+concat(empleado.nombre,' ',empleado.apellido) as Empleado, domicilio.direccion_entrega as Domicilio from domicilio,empleado,venta,cliente,venta_insumos,insumo 
+where domicilio.idventa = venta.idventa and cliente.idcliente = domicilio.idcliente and empleado.idempleado = domicilio.idempleado and 
+venta_insumos.idventa = domicilio.idventa and insumo.idinsumo = venta_insumos.idinsumo;
+
+
+-- Vista para acceder a la información de los domicilios de productos como administrador
+create view domicilios_productos_adm as select producto.nombre as Nombre, concat(cliente.nombre,' ',cliente.apellido) as Cliente, 
+concat(empleado.nombre,' ',empleado.apellido) as Empleado, domicilio.direccion_entrega as Domicilio from domicilio,empleado,venta,cliente,venta_productos,producto 
+where domicilio.idventa = venta.idventa and cliente.idcliente = domicilio.idcliente and empleado.idempleado = domicilio.idempleado and 
+venta_productos.idventa = domicilio.idventa and producto.idproducto = venta_productos.idproducto;
 
